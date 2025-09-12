0927487d1ba154ec6942464f6920fb18fbad06f3
cbysais <217733506+cbysais@users.noreply.github.com>
2025-09-12 22:50:31 +0800

    Use 'react-router'

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,12 @@
+import { Route, Routes } from "react-router";
+import LoginPage from "./pages/Login";
+
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+    </Routes>
+  );
 }
 
 export default App;
