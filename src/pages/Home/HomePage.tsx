33eb768ae0f6f3bdfd916c05bfd93dda5244345d
cbysais <217733506+cbysais@users.noreply.github.com>
2025-10-03 02:39:57 +0800

    Import SignUp and SignIn

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,7 +1,12 @@
+import SignIn from "../../components/SignIn";
+import SignUp from "../../components/SignUp";
+
 function HomePage() {
   return (
     <div>
       <span>GRP-SYPG</span>
+      <SignUp />
+      <SignIn />
     </div>
   );
 }
